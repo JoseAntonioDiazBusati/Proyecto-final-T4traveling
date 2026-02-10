4730a3217ad58a874f8cde51c4978b67026690c9
JoseAntonio <jdiabus511@g.educaand.es>
2026-02-10 12:03:52 +0100

    actualizar nuevo componente septimo commit

--- frontend/src/app/pages/news/news.component.ts
+++ frontend/src/app/pages/news/news.component.ts
@@ -1,15 +1,17 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, ActivatedRoute } from '@angular/router';
+import { Routes } from '@angular/router';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
 
-export class NewsComponent{
-  private route = inject(ActivatedRoute);
-}
+export const routes: Routes = [
+  {
+    path: '/news',
+  }
+];
