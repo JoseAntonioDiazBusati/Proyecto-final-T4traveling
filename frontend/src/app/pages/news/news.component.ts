745bc56caea60e04de98434cb975a7e389c21a25
JoseAntonio <jdiabus511@g.educaand.es>
2026-02-10 13:00:48 +0100

    actualizar componente noticia

--- frontend/src/app/pages/news/news.component.ts
+++ frontend/src/app/pages/news/news.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import {TransportService} from '../../services/transport.service';
 
 @Component({
   selector: 'app-news',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
 
-
+export class NewsComponent implements OnInit {
+  private newsService = inject();
+}
