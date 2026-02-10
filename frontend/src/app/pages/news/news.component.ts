7b16e44fd768536c4401265d692365f876fc81af
JoseAntonio <jdiabus511@g.educaand.es>
2026-02-10 11:00:40 +0100

    sexto commit

--- frontend/src/app/pages/news/news.component.ts
+++ frontend/src/app/pages/news/news.component.ts
@@ -1,20 +1,15 @@
-import { Component, OnInit, inject, signal, DestroyRef } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {RouterLink} from '@angular/router';
+import { RouterLink, ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-destination-detail',
+  selector: 'app-news',
   standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
 
-export class NewsComponent implements OnInit {
-  ngOnInit(): void {
-      throw new Error("Method not implemented.");
-  }
-  private destroyRef = inject(DestroyRef);
-
-  // Signals para estado reactivo
-  private newsItemsSignal = signal<string[]>([]);}
+export class NewsComponent{
+  private route = inject(ActivatedRoute);
+}
